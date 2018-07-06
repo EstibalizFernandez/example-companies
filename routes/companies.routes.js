3f0e7a8cfb0433c70ce963dba1be38f2828e4028
Georgina Pacey <paceygeorgina@gmail.com>
2018-07-06 22:32:18 +0200

    fix /companies/create

--- routes/companies.routes.js
+++ routes/companies.routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const companyController = require('../controllers/companies.controller.js');
 
+router.get('/create', companyController.create);
+router.post('/create', companyController.doCreate);
+
 router.get('/', companyController.list);
 
 router.get('/:code', companyController.getDetails);
 
-router.get('/create', companyController.create);
-router.post('/create', companyController.doCreate);
 router.post('/:id/delete', companyController.doDelete);
 
 module.exports = router;    
